0317fb51bcca9d94d2e995355eb2694fed855d2e
Mark Badham <markbadham@Marks-MBP.ledge.local>
2019-10-29 12:56:37 +0200

    add hellworld and numbers

---
+++ hello.groovy
@@ -0,0 +1,7 @@
+class Hello {
+    static void main(args) {
+        def message = "Hello World"
+        println(message)
+        println(message.getClass())
+    }
+}
---
+++ numbers.groovy
@@ -0,0 +1,14 @@
+class Numbers {
+    static void main(args) {
+        def age = 10
+        def bytes = 050
+        def len = 4.5
+        def nano = 3E-9
+        println(age)
+        println(age.getClass())
+        println(bytes.getClass())
+        println(len.getClass())
+        println(nano.getClass())
+        println(nano * 5)
+    }
+}
